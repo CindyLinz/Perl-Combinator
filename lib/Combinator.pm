18db82e8e2b1841fec53ef7a01eaab60389f1bb1
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2011-11-07 03:34:54 +0800

    version 0.02; fix required perl ver, remove pod-coverage test

--- lib/Combinator.pm
+++ lib/Combinator.pm
@@ -1,6 +1,6 @@
 package Combinator;
 
-use 5.006;
+use 5.010;
 use strict;
 use warnings;
 
@@ -10,11 +10,11 @@ Combinator - Intuitively write async program serially, parallel, or circularly
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
